6cc8426baea62bbb4a3b46390025749cda993404
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-07-31 16:33:00 -0300

    Cria Interface e Repositorios

---
+++ App_Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using App_Series.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
